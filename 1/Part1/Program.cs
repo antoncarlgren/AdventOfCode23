235bc90da84ee62aaaa9ebbc8ce87a4dcd075f55
Anton Carlgren <anton.carlgren@hotmail.com>
2023-12-01 20:01:54 +0100

    Day 1

---
+++ 1/Part1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine(File
+    .ReadLines("input.txt")
+    .Select(line => int.Parse($"{line.FirstOrDefault(char.IsDigit)}{line.LastOrDefault(char.IsDigit)}"))
+    .Sum());
---
+++ 1/Part2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(File
+    .ReadLines("input.txt")
+    .Select(line => new Dictionary<string, int>
+        {
+            ["zero"] = 0,
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+            ["four"] = 4,
+            ["five"] = 5,
+            ["six"] = 6,
+            ["seven"] = 7,
+            ["eight"] = 8,
+            ["nine"] = 9,
+        }
+        .Aggregate(line, (result, next) => result.Replace(next.Key, $"{next.Key[0]}{next.Value}{next.Key[^1]}"))
+        .Where(char.IsDigit))
+    .Select(line => int.Parse($"{line.FirstOrDefault()}{line.LastOrDefault()}"))
+    .Sum());
