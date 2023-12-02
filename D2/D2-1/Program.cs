678b17f67e1efaf59bedfc60017febd60613b2de
Anton Carlgren <anton.carlgren@hotmail.com>
2023-12-02 23:41:35 +0100

    Day 2

---
+++ D2/D2-1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine(File
+    .ReadLines("input.txt")
+    .Where(line => !new Regex(@"(1[3-9] red|1[4-9] green|1[5-9] blue)")
+        .IsMatch(line))
+    .Sum(game => int.Parse(new Regex(@"[0-9]+")
+        .Match(game)
+        .Value)));
---
+++ D2/D2-2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(File
+    .ReadLines("input.txt")
+    .Select(line => line
+        .Split(':', ';')[1..]
+        .Select(game => game
+            .Split(',')
+            .Select(color => new
+            {
+                Color = string.Join("", color.Where(char.IsLetter)), 
+                Count = int.Parse(string.Join("", color.Where(char.IsDigit)))
+            }))
+        .SelectMany(game => game)
+        .GroupBy(color => color.Color, color => color.Count)
+        .Select(group => group.MaxBy(count => count))
+        .Aggregate((current, next) => current * next))
+    .Sum());
