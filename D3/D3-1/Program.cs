6f1e5caede30310b6975116a2dc2844969a15ce6
Anton Carlgren <anton.carlgren@hotmail.com>
2023-12-04 23:30:14 +0100

    Day 3

---
+++ D3/D3-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+Console.WriteLine(File
+    .ReadLines("input.txt")
+    .Select((line, y) => new
+    {
+        Numbers = new Regex(@"[0-9]+")
+            .Matches(line)
+            .Select(match => new
+            {
+                match.Value,
+                Line = y,
+                Start = match.Index,
+                match.Length
+            }),
+        Gears = new Regex(@"[^0-9|.]")
+            .Matches(line)
+            .Select(match => new
+            {
+                Line = y,
+                Position = match.Index
+            }),
+    })
+    .Chunk(int.MaxValue) // ?????????? sorry
+    .Select(chunk => chunk
+        .Aggregate((current, next) => new
+        {
+            Numbers = current.Numbers.Concat(next.Numbers), 
+            Gears = current.Gears.Concat(next.Gears)
+        })
+        .Numbers
+        .Where(num => chunk
+            .SelectMany(parts => parts.Gears)
+            .Any(part => Enumerable.Range(num.Line - 1, 3).Contains(part.Line) 
+                && Enumerable.Range(num.Start - 1, num.Length + 2).Contains(part.Position)))
+        .Sum(num => int.Parse(num.Value)))
+    .First());
---
+++ D3/D3-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine(File
+    .ReadLines("input.txt")
+    .Select((line, y) => new
+    {
+        Numbers = new Regex(@"[0-9]+")
+            .Matches(line)
+            .Select(match => new
+            {
+                match.Value,
+                Line = y,
+                Start = match.Index,
+                match.Length,
+            }),
+        Gears = new Regex(@"[*]")
+            .Matches(line)
+            .Select(match => new
+            {
+                Numbers = new List<int>(),
+                Line = y,
+                Position = match.Index
+            }),
+    })
+    .Chunk(int.MaxValue) // ?????????? sorry
+    .Select(chunk => chunk
+        .Aggregate((current, next) => new
+        {
+            Numbers = current.Numbers.Concat(next.Numbers), 
+            Gears = current.Gears.Concat(next.Gears)
+        })
+        .Gears
+        .Select(gear => gear with
+        {
+            Numbers = chunk
+                .SelectMany(number => number.Numbers)
+                .Where(num => Enumerable.Range(num.Line - 1, 3).Contains(gear.Line)
+                    && Enumerable.Range(num.Start - 1, num.Length + 2).Contains(gear.Position))
+                .Select(num => int.Parse(num.Value))
+                .ToList()
+        })
+        .Where(gear => gear.Numbers.Count is 2)
+        .Select(gear => gear.Numbers[0] * gear.Numbers[1])
+        .Sum())
+    .First());
